e7ea9206444dccd6f9b597ee39cd786114dadd80
Lu Htoo Kyaw <luhtookyaw98@gmail.com>
2021-11-16 18:23:50 +0630

    Updates

---
+++ precache-manifest.683f051be55d299d59a531aadb2a15d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bbdfa5ff13907e622e348a87ba713b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4efc72ce3d2efccf997b",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4efc72ce3d2efccf997b",
+    "url": "/static/js/main.8623cdf7.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "6b787f8e89230850a2291c74eadc1c82",
+    "url": "/static/media/audrey.6b787f8e.jpg"
+  },
+  {
+    "revision": "ecb6a23c5b7a5e1d9369be46feaed41f",
+    "url": "/static/media/resume.ecb6a23c.pdf"
+  }
+]);
