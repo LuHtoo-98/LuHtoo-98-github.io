5a88982bb564092fb99602aaa0585f7677116c3a
Lu Htoo Kyaw <luhtookyaw98@gmail.com>
2021-11-17 16:30:38 +0630

    Updates

--- precache-manifest.061e3113bb412c9b4d4136c5ba73c726.js
+++ precache-manifest.061e3113bb412c9b4d4136c5ba73c726.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf0dca413c54f69881eb8e38d6db337a",
+    "revision": "65144bd1b74774bd7b6fbd82c2f407be",
     "url": "/index.html"
   },
   {
-    "revision": "c437a950c9782397c2cc",
+    "revision": "0bc222fac89a409664d9",
     "url": "/static/css/main.f5497eb8.chunk.css"
   },
   {
-    "revision": "88448ffcfce5fdaf8e09",
-    "url": "/static/js/2.cfafeff4.chunk.js"
+    "revision": "a4418edb3260995df24a",
+    "url": "/static/js/2.f96070c2.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/static/js/2.cfafeff4.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f96070c2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c437a950c9782397c2cc",
-    "url": "/static/js/main.8244cbb2.chunk.js"
+    "revision": "0bc222fac89a409664d9",
+    "url": "/static/js/main.266af557.chunk.js"
   },
   {
     "revision": "f52ba179432c3be28857",
@@ -30,5 +30,13 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ecb6a23c5b7a5e1d9369be46feaed41f",
     "url": "/static/media/resume.ecb6a23c.pdf"
+  },
+  {
+    "revision": "d6b7bb441287be66a1fdaac5a8b1032b",
+    "url": "/static/media/uit.d6b7bb44.jpeg"
+  },
+  {
+    "revision": "ba923499d1d5b4de329b4b97482d294c",
+    "url": "/static/media/universityofyangon.ba923499.jpeg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97bfcb5da368c66d06f60d6797d974ef.js"
+  "/precache-manifest.061e3113bb412c9b4d4136c5ba73c726.js"
 );
 
 self.addEventListener('message', (event) => {
