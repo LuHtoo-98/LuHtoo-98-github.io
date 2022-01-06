55171a77d8edf018af25ec9658cb3642587495d0
Lu Htoo Kyaw <luhtookyaw98@gmail.com>
2022-01-07 00:38:20 +0630

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c966cdfa312d8752baaa0036eb4371ea.js"
+  "/precache-manifest.33860d12da119db5fdd3f1e54ad5caa5.js"
 );
 
 self.addEventListener('message', (event) => {
